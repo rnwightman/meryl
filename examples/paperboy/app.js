1b34346f152662fabcb56db91b01dc67fd3c504b
Kadir Pekel <kadirpekel@gmail.com>
2010-10-14 08:42:18 -0400

    fixed damned indentation again.

--- examples/paperboy/app.js
+++ examples/paperboy/app.js
@@ -1,22 +1,22 @@
 var meryl = require('../../index');
 
 meryl
-	.p(function (req, resp, next) {
-	  require('paperboy')
-	    .deliver(__dirname, req, resp)
-    	.addHeader('Expires', 300)
-	    .error(function(statCode, msg) {
-	      next();
-	    })
-	    .otherwise(function(err) {
-	      next();
-	    });
-	})
-	.h('GET /', function(req, resp) {
-		resp.send(
-			"<h1>Welcome To NodeJS!</h1>\
-			<img src='nodejs.png' />"
-		);
-	})
-	.run();
+  .p(function (req, resp, next) {
+    require('paperboy')
+      .deliver(__dirname, req, resp)
+      .addHeader('Expires', 300)
+      .error(function(statCode, msg) {
+        next();
+      })
+      .otherwise(function(err) {
+        next();
+      });
+  })
+  .h('GET /', function(req, resp) {
+    resp.send(
+      "<h1>Welcome To NodeJS!</h1>\
+      <img src='nodejs.png' />"
+    );
+  })
+  .run();
 
