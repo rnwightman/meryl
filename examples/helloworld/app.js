8133ecc90c239ea53b8ec2a597514f1b16b0143a
Kadir Pekel <kadirpekel@gmail.com>
2010-10-14 12:37:55 -0400

    minimizw'hello wolrd' example

--- examples/helloworld/app.js
+++ examples/helloworld/app.js
@@ -1,8 +1,7 @@
 var meryl = require('../../index');
   
-meryl.h('GET /', function (req, resp) {
-  resp.send("<h1>Hello World!</h1>");
-});
+meryl
+  .h('GET /', function (req, resp) { resp.send("<h1>Hello World!</h1>"); })
+  .run();
 
-require('http').createServer(meryl.cgi()).listen(3000);
 console.log('serving http://localhost:3000');
