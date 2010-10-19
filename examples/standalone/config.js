914884563e73effd76ab5f8012d7f30da63ce47d
Kadir Pekel <kadirpekel@gmail.com>
2010-10-19 13:37:26 -0400

    added 'standalone' example and made some minor changes

---
+++ examples/standalone/config.js
@@ -0,0 +1,6 @@
+// export Meryl options
+module.exports = {
+  templateExt: '.mt', // Default is '.jshtml'
+  port: 8000
+}
+
---
+++ examples/standalone/index.mt
@@ -0,0 +1,3 @@
+<h1>Wow, this page rendered automatically</h1>
+<p><a href="/subpage">Go to subpage</a></p>
+
